fe42b6bb7b449628255b0985363697633d2f934c
ishiy <y.ishihara.1993@gmail.com>
2021-02-20 15:28:35 +0900

    Add haskell/stack/shell.nix

---
+++ haskell/stack/shell.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> {}, ghc-version ? "ghc884" }:
+let
+  ghc = pkgs.haskell.compiler.${ghc-version};
+in
+pkgs.mkShell {
+  buildInputs = [
+    ghc
+    pkgs.stack
+    pkgs.ormolu
+    pkgs.haskell-language-server
+    pkgs.haskellPackages.implicit-hie
+  ];
+
+  shellHook = ''
+    export NIX_GHC="${ghc}/bin/ghc"
+    export NIX_GHCPKG="${ghc}/bin/ghc-pkg"
+    export NIX_GHC_DOCDIR="${ghc}/share/doc/ghc/html"
+    export NIX_GHC_LIBDIR="${ghc}/lib/ghc-$($NIX_GHC --numeric-version)"
+  '';
+}
