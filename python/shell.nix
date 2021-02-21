d5e9cbd07fec7e6a1cc355c00a8ce64753e8a943
ishiy <y.ishihara.1993@gmail.com>
2021-02-21 09:13:25 +0900

    Fix python/shell.nix

--- python/shell.nix
+++ python/shell.nix
@@ -1,11 +1,14 @@
 { pkgs ? import <nixpkgs> {}, python-version ? "python3", python-libs ? (ps: []) }:
 
 let
-  py-env = pkgs.${python-version}.withPackages python-libs;
+  py = pkgs.${python-version};
+  py-env = py.withPackages python-libs;
 in
 pkgs.mkShell {
   buildInputs = [
     py-env
-    pkgs.python-language-server
+    py.pkgs.python-language-server
+    py.pkgs.jedi
+    py.pkgs.yapf
   ];
 }
